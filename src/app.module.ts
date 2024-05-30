91b931efd0e2c6762dd03e9a3002347e1ad80314
filipgasinski <filip.gasinski2022@gmail.com>
2024-05-30 16:25:37 +0200

    database_connection_ENV_insteadof_URL

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,10 +8,18 @@ import { moviesController } from './movies/movies.controller';
 import { MoviesService } from './movies/movies.service';
 import { moviesModule } from './movies/movies.module';
 import { Movie, MovieSchema } from './movies/movies.model';
+import * as dotenv from 'dotenv';
+
+dotenv.config()
+
+const mongoUrl = process.env.MONGODB_URL
+if (!mongoUrl) {
+  throw new Error('MONGODB_URL is not defined')
+}
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb+srv://dbUser:haslo123@cluster0.tfbwlcs.mongodb.net/'), // Database connection
+    MongooseModule.forRoot(mongoUrl), // Database connection
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
     MongooseModule.forFeature([{ name: Movie.name, schema: MovieSchema }]),
     UserModule,
--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,6 @@ import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-const apiKey: string | undefined = process.env.OMDB_API_KEY;
-if(!apiKey) {
-  throw new Error('OMDB API KEY is not defined')
-}
-console.log(`You API key is: ${apiKey}`)
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
